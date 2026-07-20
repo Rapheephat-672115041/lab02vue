19b6327346f55a5885d35f4430f80c1c34da3932
Rapheephat <rapheephat_a@cmu.ac.th>
2026-07-20 16:42:39 +0700

    checkpoint 8.4 & 8.7
    um....

---
+++ catacombvue02/src/components/EventDetails.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { Event } from '@/types'
+
+defineProps<{
+  event: Event
+}>()
+</script>
+
+<template>
+  <div class="event-details">
+    <div>{{ event.category }}</div>
+    <div>{{ event.organizer }}</div>
+  </div>
+</template>
+
+<style scoped>
+.event-details {
+  text-align: right;
+}
+</style>
