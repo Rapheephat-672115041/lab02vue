a147bb44963df5e238f7cfcdfc753c7cb48540a5
Rapheephat <rapheephat_a@cmu.ac.th>
2026-08-10 17:12:57 +0700

    commit for good measure
    polished, i think

--- catacombvue02/src/App.vue
+++ catacombvue02/src/App.vue
@@ -9,14 +9,17 @@ const { message } = storeToRefs(store)
 <template>
   <div class="text-center font-sans text-gray-700 antialiased">
     <header>
-      <div id="flashMessage" v-if="message">
+      <div id="flashMessage" class="animate-fade" v-if="message">
         <h4>{{ message }}</h4>
       </div>
       <div class="wrapper">
         <nav>
-          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
-          <RouterLink :to="{ name: 'student-list-view' }">Student</RouterLink> |
-          <RouterLink :to="{ name: 'about' }">About</RouterLink>
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500"
+          :to="{ name: 'event-list-view' }">Event</RouterLink> |
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500"
+          :to="{ name: 'student-list-view' }">Student</RouterLink> |
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500"
+          :to="{ name: 'about' }">About</RouterLink>
         </nav>
       </div>
     </header>
@@ -30,29 +33,7 @@ nav {
   padding: 30px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-
 h2 {
   font-size: 20px;
-}
-
-@keyframes yellowFade {
-  from {
-    background-color: yellow;
-  }
-  to {
-    background-color: transparent;
-  }
-}
-
-#flashMessage {
-  animation: yellowFade 3s ease-in-out;
 }
 </style>
