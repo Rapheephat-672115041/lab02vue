228b1bd9be135a1ae6e7ccef98f9c5b9fa3db092
Rapheephat <rapheephat_a@cmu.ac.th>
2026-08-10 13:24:26 +0700

    (Lab03) checkpoint 4.2

--- catacombvue02/src/views/event/RegisterView.vue
+++ catacombvue02/src/views/event/RegisterView.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
 import type { Event } from '@/types'
+import { toRefs } from 'vue'
+import { useRouter } from 'vue-router'
 
-defineProps<{
+ const props =defineProps <{
   event: Event
 }>()
+
+const { event } = toRefs(props)
+const router = useRouter() 
+
+const register = () => {
+  router.push({ name: 'event-detail-view', params: { id: event.value.id } })
+}
 </script>
 
 <template>
   <div v-if="event">
-    <p>Register event here[cite: 9]</p>
+    <p>Register event here</p>
+
+    <button @click="register">Register</button>
   </div>
 </template>
