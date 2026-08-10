8f8cefbf4eafb063750b8fd3de57979b1a894cb4
Rapheephat <rapheephat_a@cmu.ac.th>
2026-08-10 13:38:54 +0700

    (Lab03) checkpoint 5.2

--- catacombvue02/src/router/index.ts
+++ catacombvue02/src/router/index.ts
@@ -7,6 +7,7 @@ import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetWorkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -47,6 +48,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView
+    },
+    {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView
     },
     {
       path: '/404/:resource',
